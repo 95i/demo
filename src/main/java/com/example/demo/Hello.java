2e30029da629309baee368621f1fc1abb1e59e5a
614af13e6588d30502651cd0 <accounts_614af13e05cc3e002d2fb39b@mail.teambition.com>
2021-11-09 17:52:33 +0800

    更新

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -2,6 +2,6 @@ package com.example.demo;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("asdf000000000000");
+        System.out.println("asdf0000000");
     }
 }
