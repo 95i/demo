1eb4a52ef93eeccbd1d3745d73a1eab43f2ac555
614af13e6588d30502651cd0 <accounts_614af13e05cc3e002d2fb39b@mail.teambition.com>
2021-11-15 11:40:35 +0800

    更新

--- src/main/java/com/example/demo/A20211115.java
+++ src/main/java/com/example/demo/A20211115.java
@@ -1,7 +1,56 @@
 package com.example.demo;
 
+import java.util.Scanner;
+
 public class A20211115 {
     public static void main(String[] args) {
+//        int a,b,c,max;
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("请输入a");
+//        a = scanner.nextInt();
+//
+//        System.out.print("请输入b");
+//        b = scanner.nextInt();
+//
+//        System.out.print("请输入c");
+//        c = scanner.nextInt();
+//
+//
+//        if (a>b){
+//            max = a;
+//        }else {
+//            System.out.println("");
+//        }
 
+        Scanner c = new Scanner(System.in);
+        System.out.println("请输入3个数");
+        int a = c.nextInt();
+        int b = c.nextInt();
+        int s = c.nextInt();
+        System.out.println(a + "," + b + "," + s);
+
+        int temp;
+        if (a < b) {//b此时是temp。a是b/
+            temp = a;
+            a = b;
+            b = temp;
+
+        }
+
+        if (a < s) {
+            temp = a;
+            a = s;
+            s = temp;
+
+        }
+        if (b < s) {
+            temp = b;
+            b = s;
+            s = temp;
+        }
+
+        System.out.println("结果：" + a + ">" + b + ">" + s);
     }
+
 }
