4b8d74484af0942987cb4b0c86381a47546dfde9
614af13e6588d30502651cd0 <accounts_614af13e05cc3e002d2fb39b@mail.teambition.com>
2021-11-20 21:08:10 +0800

    更新

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 public class Hello {
     public static void main(String[] args) {
-        String aaa = "asdf";
-        System.out.println("yyyyyyyyyyyyyy" + aaa);
+
     }
 }
---
+++ src/main/java/com/example/demo/Z544_1.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import java.util.Scanner;
+
+public class Z544_1 {
+    public static void main(String[] args) {
+        System.out.print("请输入成绩：");
+        Scanner scanner = new Scanner(System.in);
+        int score = scanner.nextInt();
+        int key = score/10;
+        System.out.println(key);
+
+        switch (key) {
+            case 10:
+            case 9:
+                System.out.println("优");
+                break;
+            case 8:
+                System.out.println("良");
+                break;
+            case 7:
+                System.out.println("中");
+                break;
+            case 6:
+                System.out.println("及格");
+                break;
+            default:
+                System.out.println("不及格");
+                break;
+        }
+    }
+}
