3abbf0385e7e516d187fab16b1b0b726ec3e08e8
614af13e6588d30502651cd0 <accounts_614af13e05cc3e002d2fb39b@mail.teambition.com>
2021-11-21 12:51:02 +0800

    更新

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -1,7 +1,35 @@
 package com.example.demo;
 
-public class Hello {
-    public static void main(String[] args) {
-
-    }
-}
+import java.util.*;
+public class Hello{
+    public static void main(String[] args){
+        Scanner rank=new Scanner(System.in);
+//        flost grade;
+        int grade;
+        System.out.print("请输入学生成绩:");
+//        grade=rank.nextFlost();
+        grade = rank.nextInt();
+        if(grade<0||grade>100) {
+            System.out.println("输入出错,请重新输入:");
+        }else {
+//            switch(grade/10){
+            switch(grade/10){
+                case 10:
+                case 9:
+                    System.out.println("成绩为优");
+                    break;
+                case 8:
+                    System.out.println("成绩为良");
+                    break;
+                case 7:
+                    System.out.println("成绩为中");
+                    break;
+                case 6:
+                    System.out.println("成绩为及");
+                    break;
+                default:
+                    System.out.println("成绩为不及格");
+                    System.out.println("学生成绩为:+grade(f)");
+            }
+        }
+    }}
