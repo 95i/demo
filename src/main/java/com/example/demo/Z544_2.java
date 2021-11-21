481ef4c8bc4829b9f031060b9229f16ab18e81cb
614af13e6588d30502651cd0 <accounts_614af13e05cc3e002d2fb39b@mail.teambition.com>
2021-11-21 20:14:17 +0800

    更新

--- src/main/java/com/example/demo/Z544_2.java
+++ src/main/java/com/example/demo/Z544_2.java
@@ -7,7 +7,7 @@ public class Z544_2 {
         double score;
         int sco;
         Scanner reader=new Scanner(System.in);
-        System.out.print("Please enter score: ");
+        System.out.print("请输入成绩: ");
         score=reader.nextDouble();
         sco=(int)score/10;
         switch(sco)
@@ -18,23 +18,23 @@ public class Z544_2 {
             case 3:
             case 4:
             case 5:
-                System.out.println("The grade is D!");
+                System.out.println("成绩为D!");
                 break;
             case 6:
-                System.out.println("The grade is C!");
+                System.out.println("成绩为C!");
                 break;
             case 7:
-                System.out.println("The grade is B!");
+                System.out.println("成绩为B!");
                 break;
             case 8:
                 if(score/10<=8.4)
-                    System.out.println("The grade is B!");
+                    System.out.println("成绩为B!");
                 else
-                    System.out.println("The grade is A!");
+                    System.out.println("成绩为A!");
                 break;
             case 9:
             case 10:
-                System.out.println("The grade is A!");
+                System.out.println("成绩为A!");
                 break;
             default:
                 break;
