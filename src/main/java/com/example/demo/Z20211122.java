d883843da76c26cc5ed52ed214a7d3592435b39a
614af13e6588d30502651cd0 <accounts_614af13e05cc3e002d2fb39b@mail.teambition.com>
2021-11-23 22:16:39 +0800

    更新

--- src/main/java/com/example/demo/Z20211122.java
+++ src/main/java/com/example/demo/Z20211122.java
@@ -2,17 +2,12 @@ package com.example.demo;
 
 public class Z20211122 {
     public static void main(String[] args) {
-        int sum=0;
-        int i=0;
-        while(i<100)
-        {
-            if(i%2==0) {
-                sum += i;
-                System.out.println(i);
-            }
-            i++;
+        int sum = 0;
+        int i = 0;
+        while (i < 100) {
+            sum = sum + i;
+            i = i + 2;
         }
         System.out.println(sum);
-
     }
 }
