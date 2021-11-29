0af6d584b4a7bfabed2aa54e8b4fd53fad17805b
614af13e6588d30502651cd0 <accounts_614af13e05cc3e002d2fb39b@mail.teambition.com>
2021-11-29 11:21:33 +0800

    更新

--- src/main/java/com/example/demo/A21211129.java
+++ src/main/java/com/example/demo/A21211129.java
@@ -5,7 +5,7 @@ public class A21211129 {
         for (int i = 1; i <= 9; i++) {
 //            System.out.print(i);
             for (int j = 1; j <= 9; j++) {
-                System.out.print(i+"*"+j+"="+i*j+"   ");
+                System.out.print(i + "*" + j + "=" + i * j + "\t");
 //                break;
             }
             System.out.println();
