6de975788ba627a48fd5420288da7ec8d1bff3bb
614af13e6588d30502651cd0 <accounts_614af13e05cc3e002d2fb39b@mail.teambition.com>
2021-12-06 10:50:05 +0800

    更新

--- src/main/java/com/example/demo/Z712.java
+++ src/main/java/com/example/demo/Z712.java
@@ -4,23 +4,23 @@ import java.util.Scanner;
 
 public class Z712 {
     public static void main(String[] args) {
-            int num;
-            Scanner reader=new Scanner(System.in);
-            System.out.print("Please enter length: ");
-            num=reader.nextInt();
-            int[] n=new int[num];
-            int i, sum=0;
-            float aver;
-            System.out.print("Please enter "+num+" numbers: ");
-            for(i=0; i<num; i++) {
-                n[i]=reader.nextInt();
-                sum+=n[i];
-            }
-            aver=(float)sum/num;
-            System.out.println("Average="+aver);
-            System.out.print("Greater than average numbers: ");
-            for(i=0; i<num; i++)
-                if(n[i]>aver)
-                    System.out.print(" "+n[i]+" ");
+        int num;
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Please enter length: ");
+        num = reader.nextInt();
+        int[] n = new int[num];
+        int i, sum = 0;
+        float aver;
+        System.out.print("Please enter " + num + " numbers: ");
+        for (i = 0; i < num; i++) {
+            n[i] = reader.nextInt();
+            sum += n[i];
+        }
+        aver = (float) sum / num;
+        System.out.println("Average=" + aver);
+        System.out.print("Greater than average numbers: ");
+        for (i = 0; i < num; i++)
+            if (n[i] > aver)
+                System.out.print(" " + n[i] + " ");
     }
 }
