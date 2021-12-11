3a09883a8da009ae65ca176ef81486f6a2e8def9
614af13e6588d30502651cd0 <accounts_614af13e05cc3e002d2fb39b@mail.teambition.com>
2021-12-11 11:48:37 +0800

    更新

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 public class Student {
     String name;
+    int age;
     int chinese;
     int math;
     int english;
@@ -17,12 +18,13 @@ public class Student {
     public static void main(String[] args) {
         Student student = new Student();
         student.name = "张三";
+        student.age = 18;
         student.english = 60;
         student.math = 60;
         student.chinese = 70;
-        System.out.println(student.name);
-        System.out.println(student.total());
-        System.out.println(student.average());
+        System.out.println(student.name + "\n" + student.age+"岁");
+        System.out.println("三门成绩总和:"+student.total());
+        System.out.println("平均值:"+student.average());
     }
 
 }
