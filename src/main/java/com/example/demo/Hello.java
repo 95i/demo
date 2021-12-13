a73d8fe8fa14ee35275807f357e461855c658190
614af13e6588d30502651cd0 <accounts_614af13e05cc3e002d2fb39b@mail.teambition.com>
2021-12-13 11:11:22 +0800

    更新

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -5,14 +5,34 @@ import com.smtxi.Student;
 
 public class Hello {
     public static void main(String[] args) {
-        Student student = new Student();
-        student.name = "张三";
-        student.age = 18;
-        student.english = 60;
-        student.math = 60;
-        student.chinese = 70;
-        System.out.println(student.name + "\n" + student.age + "岁");
-        System.out.println("三门成绩总和:" + student.total());
-        System.out.println("平均值:" + student.average());
+        Student zhangsan = new Student();
+        Student lisi = new Student();
+        zhangsan.name = "张三";
+        zhangsan.age = 18;
+        zhangsan.english = 60;
+        zhangsan.math = 60;
+        zhangsan.chinese = 90;
+        zhangsan.zhangsan();
+        System.out.println("三门成绩总和:" + zhangsan.total());
+        System.out.println("平均值:" + zhangsan.average());
+
+        lisi.name = "李四";
+        lisi.age = 20;
+        lisi.english = 80;
+        lisi.math = 30;
+        lisi.chinese = 90;
+        lisi.lisi();
+        System.out.println("三门成绩总和:" + lisi.total());
+        System.out.println("平均值:" + lisi.average());
+
+        if (lisi.total() < zhangsan.total()) {
+            System.out.println(zhangsan.name + "的成绩好");
+        } else if (lisi.total() > zhangsan.total()) {
+            System.out.println(lisi.name + "的成绩好");
+        } else {
+            System.out.println(zhangsan.name + "和" + lisi.name + "的成绩好");
+        }
     }
+
 }
+
